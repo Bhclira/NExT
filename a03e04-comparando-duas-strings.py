96ac928b2e8268b55f21161031d6755226895b4e
Rivotr33 <bhcl@cesar.school>
2022-03-27 15:00:09 -0300

    a03e04-comparando-duas-strings

---
+++ a03e04-comparando-duas-strings.py
@@ -0,0 +1,9 @@
+# Crie um programa que compara duas strings.
+
+txt = input('Digite um texto: ')
+texto = input('Digite outro texto: ')
+
+if txt==texto:
+    print(f'{txt} e {texto} são iguais')
+else:
+    print(f'{txt} e {texto} são diferentes')
