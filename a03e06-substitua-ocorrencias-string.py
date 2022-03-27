b1a7bf3c07510be003edb9b1fb6af02ab429873c
Rivotr33 <bhcl@cesar.school>
2022-03-27 15:22:48 -0300

    Create a03e06-substitua-ocorrencias-string.py

---
+++ a03e06-substitua-ocorrencias-string.py
@@ -0,0 +1,6 @@
+# Escreva um programa que substitui as ocorrencias de um caractere a em uma string por outro caractere b.
+
+txt = input('Digite uma palavra: ')
+
+print(txt.replace('e', 'u'))
+# .replace ('paramentro1', 'parametro2') substitui dentro da string o parametro1 pelo parametro 2
