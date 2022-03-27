6999d346f4facd8c48e3a38f85398f27a0ad479b
Rivotr33 <bhcl@cesar.school>
2022-03-27 17:31:01 -0300

    delete arqs

--- FP_AULA10_ex02 - divisao-inteira-divisao-real.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input('Digite numero A:'))
-num2 = int (input('Digite numero B: '))
-
-if num1%num2 == 0:
-    print('Divisão inteira.')
-else:
-    print('Divisão real.')
--- FP_AULA10_ex04 - park-se-entradas-por-idade.py
+++
@@ -1,8 +0,0 @@
-idade = int(input("Digite sua idade: "))
-
-if (idade < 4) or (idade > 60):
-    print('Entrada gratuita')
-elif idade > 4 and idade < 18:
-    print ("A estrada custa R$ 20")
-else:
-    print ("O valor do ingresso é R$ 30")
