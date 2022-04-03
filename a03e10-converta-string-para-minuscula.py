fb50537f613c5020ee5a8e7e6cb9cbc4e09e7dd3
brenomanDEV <bhcl@cesar.school>
2022-04-03 11:25:00 -0300

    add a03e10-converta-string-para-minuscula

---
+++ a03e10-converta-string-para-minuscula.py
@@ -0,0 +1,7 @@
+# Faça um programa para converter uma letra maiuscula em letra minuscula. Use a tabela ASCII para resolver o problema.
+
+letra = input('\nDigite uma letra: ')
+
+letra = letra.lower()
+
+print(f'\nConversão em minúscula: {letra}\n')
