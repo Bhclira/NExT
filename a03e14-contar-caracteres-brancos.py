b891ddb22c515b70e040d802c83cef72816ffe47
brenomanDEV <bhcl@cesar.school>
2022-04-03 12:26:08 -0300

    Create a03e14-contar-caracteres-brancos.py

---
+++ a03e14-contar-caracteres-brancos.py
@@ -0,0 +1,7 @@
+# Ler uma frase e contar quantos caracteres sao brancos. Lembre-se que uma frase e um conjunto de caracteres (vetor).
+
+frase = 'o rato roeu a roupa do rei de roma'
+
+print(f'Número encontrado de espaços em branco: {frase.count(" ")}')
+
+# error - se você usa aspas simples para abrir o print formatado, precisa usar aspas duplas no seu interior.
