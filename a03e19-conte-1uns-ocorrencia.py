993d38fe01af4a488f3a8c13487a4596b4cddcce
brenomanDEV <bhcl@cesar.school>
2022-04-03 12:43:17 -0300

    Create a03e19-conte-1uns-ocorrencia.py

---
+++ a03e19-conte-1uns-ocorrencia.py
@@ -0,0 +1,5 @@
+# Faça um programa que conte o numero de 1's que aparecem em um string.
+
+texto = '1uhi1uh38138184y84141841y8y19814'
+
+print(f'\nQuantidade de ocorrências para o número 1: {texto.count("1")} ocorrências')
