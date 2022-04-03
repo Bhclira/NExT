0051581b684451f38affc5703a98aaead3e311dc
brenomanDEV <bhcl@cesar.school>
2022-04-03 13:43:10 -0300

    Create a04e01-lista-maior-numero.py

---
+++ a04e01-lista-maior-numero.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia 5 números e informe o maior número.
+
+numeros = [1, 4, 7, 8, 0]
+
+print(f'\nPara a lista: {numeros}')
+
+print(f'\nO maior número da lista: {max(numeros)}')
+
+print(f'O menor número da lista: {min(numeros)}\n')
