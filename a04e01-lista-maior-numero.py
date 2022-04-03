17766940e502183123968fac1ed6093a2dd7b8c7
brenomanDEV <bhcl@cesar.school>
2022-04-03 13:53:00 -0300

    Update a04e01-lista-maior-numero.py

--- a04e01-lista-maior-numero.py
+++ a04e01-lista-maior-numero.py
@@ -7,3 +7,14 @@ print(f'\nPara a lista: {numeros}')
 print(f'\nO maior número da lista: {max(numeros)}')
 
 print(f'O menor número da lista: {min(numeros)}\n')
+
+# jeito 2
+
+numero = []
+
+for i in range(0,5):
+    numero.append(int(input('Insira um Número: ')))
+
+print(f'\nlista de inteiros obtidos: {numero}\n')
+print(f'\nO maior número da lista: {max(numero)}')
+print(f'O menor número da lista {min(numero)}\n\n')
