111a06040f9a1549a50088bced572febe1b583cd
brenomanDEV <bhcl@cesar.school>
2022-04-06 11:45:05 -0300

    Create a05aprof01-faca-funcao-fatorial.py

---
+++ a05aprof01-faca-funcao-fatorial.py
@@ -0,0 +1,21 @@
+# Faça uma função que receba um valor inteiro e positivo e calcule o seu fatorial.
+
+num = int(input('\nDigite Número Inteiro e Positivo: '))
+
+contador = 1
+
+
+def calcule_fatorial(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n > 1:
+        resultado = 1
+        for i in range(1,n+1):
+            resultado = resultado * i
+        return resultado
+        
+          
+print('\nResultado Fatorial para {num}: ')
+print(calcule_fatorial(num))
