3710d6005a3bea8ba1e22b34e802e9a9e14ea88e
brenomanDEV <bhcl@cesar.school>
2022-05-03 18:46:52 -0300

    Update a03e02-receba-string-mostre-seu-tamanho.py

--- a03e02-receba-string-mostre-seu-tamanho.py
+++ a03e02-receba-string-mostre-seu-tamanho.py
@@ -1,4 +1,45 @@
-# Crie um programa que imprima o comprimento de uma string
+'''
+Crie um programa que:
 
-txt = input('Escreva um nome: ')
+    a) imprima o comprimento de uma STRING
+    b) Substitui as ocorrencias de uma substring fornecida pelo usuário por outra encontrada na string original
+    c) Converta substrings da sting principal para minúsculas
+    d) Por fim, imprima intervalo de index 0 a index 4.
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+txt = input('Escreva uma frase qualquer: ')
+
+# LETRA A
 print(len(txt))
+
+#LETRA B
+print(txt.replace('e', 'u'))
+    
+'''
+    .replace ('paramentro1', 'parametro2') substitui dentro da
+        string o parametro1 pelo parametro 2
+'''
+
+# LETRA C
+print(f'String em Minúscula: {txt.lower()}')
+
+# LETRA D
+print(f'Intervalo de index solicitado: {txt[0:4]}')
