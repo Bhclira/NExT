192353a139089d0fed003bfb7e2523aa02a0a737
brenomanDEV <bhcl@cesar.school>
2022-05-25 09:06:22 -0300

    Create ex09-exerciciodeCasa.py

---
+++ FundProg/Aula21-Arquivos/ex09-exerciciodeCasa.py
@@ -0,0 +1,10 @@
+'''
+Desenvolva um programa de cadastrar alunos, o programa deve registrar nome, email e curso do aluno. Cada novo registro deve ser armazenado em arquivo. O usuário deve ter as seguintes opções:
+
+ Cadastrar novo aluno;
+ Listar alunos cadastrados;
+ Buscar aluno;
+ Remover aluno
+
+'''
+
