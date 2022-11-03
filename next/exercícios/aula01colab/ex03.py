d3ce6c32739d71e3f50de5d309c636f8289ae1b5
brenomanDEV <bhcl@cesar.school>
2022-11-03 18:41:21 -0300

    Create ex03.py

---
+++ next/exercícios/aula01colab/ex03.py
@@ -0,0 +1,14 @@
+'''
+ex03 - Faça um Programa que peça dois números e imprima a soma
+'''
+
+num1 = float(input('Digite um primeiro número: '))
+num2 = int(input('Digite um segundo número: '))
+
+resultado = num1 + num2
+
+print('A soma dos valores é ' + str(resultado))
+print('A soma dos valores é: %i' %(resultado))
+print(f'A soma dos valores é: {resultado}')
+print(int(num1 + num2))
+print(resultado)
