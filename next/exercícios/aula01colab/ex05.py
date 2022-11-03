ba6586204941d4ea6427525dc07883f11018a560
brenomanDEV <bhcl@cesar.school>
2022-11-03 18:58:15 -0300

    Create ex05.py

---
+++ next/exercícios/aula01colab/ex05.py
@@ -0,0 +1,11 @@
+'''
+ex05 - Faça um Programa que converta metros para centímetros
+'''
+
+print('Bem-vindos ao Conversor de medidas METRO-Centímetro/n')
+
+medida = float(input('Digite uma medida [em metros] para conversão: '))
+
+resultado = medida*100
+
+print(f'A medida em centímetros calculada foi de: {resultado}')
