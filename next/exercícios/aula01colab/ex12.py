0485a5209c9fcfbb5b934528b6af7277e33252df
brenomanDEV <bhcl@cesar.school>
2022-11-04 19:19:00 -0300

    Create ex12.py

---
+++ next/exercícios/aula01colab/ex12.py
@@ -0,0 +1,11 @@
+'''
+ex12 - Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal
+
+fórmula: (72.7*altura) - 58
+'''
+
+altura = float(input('\nPara calcular seu peso ideal, digite sua altura: '))
+
+pesoIdeal = (72.7*altura) - 58
+
+print(f'Seu peso ideal é de: {pesoIdeal}\n')
