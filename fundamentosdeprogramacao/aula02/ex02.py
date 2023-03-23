fbd6c7d9c2a7ea77518b1ee5f231ec0c5f093b7e
brenomanDEV <bhcl@cesar.school>
2023-03-23 08:49:37 -0300

    Create ex02.py

---
+++ fundamentosdeprogramacao/aula02/ex02.py
@@ -0,0 +1,11 @@
+num1 = int(input('Digite um primeiro número: '))
+num2 = int(input('Digite um primeiro número: '))
+
+soma = num1 + num2
+
+if soma > 5:
+    print(f'Resultado = {soma + 5}')
+
+elif soma <= 10:
+    print(f'Resultado = {soma - 5}')
+    
