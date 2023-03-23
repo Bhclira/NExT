0ac53ace7d5cb2be780a553d24b3cc9cffb9cc54
brenomanDEV <bhcl@cesar.school>
2023-03-23 08:55:00 -0300

    Update ex02.py

--- fundamentosdeprogramacao/aula02/ex02.py
+++ fundamentosdeprogramacao/aula02/ex02.py
@@ -1,11 +1,10 @@
 num1 = int(input('Digite um primeiro número: '))
-num2 = int(input('Digite um primeiro número: '))
+num2 = int(input('Digite um segundo número: '))
 
 soma = num1 + num2
 
-if soma > 5:
-    print(f'Resultado = {soma + 5}')
+if soma > 10:
+    print(f'\nResultado = {soma + 5}')
 
 elif soma <= 10:
-    print(f'Resultado = {soma - 5}')
-    
+    print(f'\nResultado = {soma - 5}')
