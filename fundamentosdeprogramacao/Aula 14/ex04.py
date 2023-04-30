7698cd4f6add1feaf9ea4eb05b95adaae5b0dc7b
brenomanDEV <bhcl@cesar.school>
2023-04-30 11:33:08 -0300

    Update ex04.py

--- fundamentosdeprogramacao/Aula 14/ex04.py
+++ fundamentosdeprogramacao/Aula 14/ex04.py
@@ -12,3 +12,61 @@ while True:
         break
 
 print(f'Valor da lista encontrado na posição 2 foi: {vetor[2]}')
+
+while True:
+    print()
+    print(vetor)
+    print()
+    print('Você pode manipular sua lista a partir das opções abaixo: \n')
+    print('[1] Substituir valor Existente: ')
+    print('[2] Adicionar novo valor: ')
+    print('[3] Saber quantidade de valores do vetor: ')
+    print('[4] Adicionar um valor em um índice específico: ')
+    print('[5] Remover valor pelo seu índice: ')
+    print('[6] Remover um valor pelo seu conteúdo: ')
+    print()
+
+    
+    opc = int(input('Digite a opção desejada: '))
+
+    if opc == 1:
+        
+        index = int(input(f'Digite o índice do valor a ser substituído: '))
+        new_value = int(input(f'Digite o valor novo: '))
+        vetor[index] = new_value
+        print(vetor)
+    
+    elif opc == 2:
+        new_value = (int(input('Digite o número inteiro a acrescentar: ')))
+        vetor.append(new_value)
+        print(vetor)
+    
+    elif opc == 3:
+        print(f'A quantidade de entidades do vetor é de: {len(vetor)}')
+            
+    elif opc == 4:
+        
+        index = int(input('Digite a posição em que deseja anexar o novo valor: '))
+        new_value = int(input('Digite o novo valor: '))
+        vetor.insert(index, new_value)
+        print(vetor)
+    
+    elif opc == 5:
+        index = int(input('Digite o índice do valor que deseja remover: '))
+        vetor.pop(index)
+        print(vetor)
+
+    elif opc == 6:
+        value = int(input('Digite o novo valor a ser substituido: '))
+        vetor.remove(value)
+        print(vetor)
+    
+    else:
+        print('Digite um número válido entre 1 e 6!')
+
+
+    alterar = str(input('\nSe deseja MODIFICAR + algo, escolha a opção (s/n): '))
+
+    if alterar in 'nN':
+        print('\n... PROGRAMA FINALIZADO ...\n')
+        break
